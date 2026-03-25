e2aaf31665bf7ae847ae40d89050d19e72979da0
VEEETOOORRR <antonni2601.ag@gmail.com>
2026-03-25 15:22:57 -0300

    relatório de células funcional

--- tarefas.tcl
+++ tarefas.tcl
@@ -1,1 +1,20 @@
+set fp [open "contador_netlist.v" r]
+set file_data [read $fp]
+close $fp
 
+set AND2 0
+set XOR2 0
+set flipflop_D 0
+set total 0
+
+set AND2 [llength [regexp -all -inline {AND2\s+\w+} $file_data]]
+set XOR2 [llength [regexp -all -inline {XOR2\s+\w+} $file_data]]
+set flipflop_D [llength [regexp -all -inline {flipflop_D\s+\w+} $file_data]]
+
+set total [expr $AND2 + $XOR2 + $flipflop_D]
+
+puts "=== RELATÓRIO DE CÉLULAS ==="
+puts "AND2:        $AND2 instâncias"
+puts "XOR2:        $XOR2 instâncias"
+puts "flipflop_D:  $flipflop_D instâncias"
+puts "Total:       $total instâncias" 
